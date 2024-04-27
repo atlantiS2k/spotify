d123b2fd69b5727c163b4ac70ea89dcfe42db73a
Rusya <115230795+Ruslan321532@users.noreply.github.com>
2024-04-27 21:08:47 +0600

    feat/home-ui (#3)
    * config(husky): add husky config
    
    * feat(home-ui): add home ui

---
+++ config/sites.ts
@@ -0,0 +1,7 @@
+export const siteConf = {
+  name: 'Spotify',
+  description: 'Listen Music to Spotify',
+  creator: 'Ruslan Buralkiev',
+  publisher: 'Ruslan Buralkiev',
+  applicationName: 'Spotify Clone',
+};
